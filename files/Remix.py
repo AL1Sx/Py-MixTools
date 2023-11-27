fbf60c324061bd443ab474ff2185d61046de19c2
Nekona Alice <125648566+AL1Sx@users.noreply.github.com>
2023-11-27 16:29:17 +0800

    Update Remix.py

--- files/Remix.py
+++ files/Remix.py
@@ -8,29 +8,29 @@ import time
 #import base64
 
 print("RemixTools")
-print("版本号:Alpha0")
+print("版本号:Alpha1")
 print("键入[H]以获取命令的帮助,请在出现[>>>]后再键入相关命令.")
-print("")
+print("---")
 
 #头
 while True:
     time.sleep(1)
     insert = input("主页>>>")
     if 'H' in insert or 'h' in insert:
-        print('')
+        print('---')
         print('[S]使用搜索引擎搜索.')
         print('[E]使用简单字符工具')
         print('[H]再次弹出帮助菜单')
         print('[A]关于本程序')
         print('[Q]退出本程序')
-        print('')
+        print('---')
     
 
     
 #搜索功能    
     elif 'S' in insert or 's' in insert:
-        print('请选择你所想要的搜索引擎')
-        print('[B]国际必应 [G]谷歌 [任意]退出本界面 [QUIT]在搜索功能内退出')
+        print('请选择你所想要的搜索引擎:')
+        print('[B]必应 [G]谷歌 [任意]返回 [QUIT]在搜索功能内退出')
         search = input("选择>>>")
         print('')
         while True:
@@ -62,12 +62,33 @@ while True:
 
 #字符转换
     elif 'E' in insert or 'e' in insert:
-        print('没做呢')
+        print('请选择你所想要的字符转换功能:')
+        print('[F]检查单个字符 [任意]返回 [QUIT]在功能内退出')
+        exchange = input('选择>>>')
+        print('---')
+        while True:
+            if 'F' in exchange or 'f' in exchange:
+                word = input('单个字符>>>')
+                print('---')
+                if 'QUIT' in word:
+                    print('已退出转换功能.')
+                    print('---')
+                    break
+                else:
+                    font = ord(word)
+                    print('字符:' + word )
+                    print(word + '的16进制为:', hex(font))
+                    print(word + '的8进制为:', oct(font))
+                    print(word + '的2进制为:', bin(font))
+                    print('---')
+
+
 
 
 #关于
     elif 'A' in insert or 'a' in insert:
-        print('没做呢')
+        print('关注冥兮谢谢喵')
+        url = '114514.com'
         
 #退出
     elif 'Q' in insert or 'q' in insert:
